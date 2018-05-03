fadba582e027aa1881bbb0a201abfc99e3a6498c
Daniel <danielr.dr81@gmail.com>
2018-05-03 12:58:50 +0200

    Slice Layout gelegt und getestet

--- BP-OpenDiabetesVault-GUI-master/src/opendiabetesvaultgui/slice/SliceController.java
+++ BP-OpenDiabetesVault-GUI-master/src/opendiabetesvaultgui/slice/SliceController.java
@@ -5,10 +5,44 @@
  */
 package opendiabetesvaultgui.slice;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import opendiabetesvaultgui.launcher.FatherController;
+
 /**
  *
  * @author Daniel Schäfer, Martin Steil, Julian Schwind, Kai Worsch
  */
-public class SliceController {
+public class SliceController extends FatherController implements Initializable{
+    
+    @FXML
+    private ListView<String> listviewfilterelements ;
+    
+    
+    @FXML
+    private void doFilter(ActionEvent event){
+        
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        //addItems to listview
+        ObservableList<String> items = listviewfilterelements.getItems();
+        items.add("Test");
+        items.add("Test1");
+        items.add("Test2");
+        items.add("Test3");
+        items.add("Test4");
+        items.add("Test5");
+        
+    }
+
+    
+    
 }
