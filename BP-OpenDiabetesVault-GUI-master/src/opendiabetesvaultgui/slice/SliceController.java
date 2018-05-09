7a460cb8cc5253af39a1ddd2a76f30fcc29df5f3
Daniel <danielr.dr81@gmail.com>
2018-05-09 13:01:38 +0200

    Drag and Drop Feature angefangen

--- BP-OpenDiabetesVault-GUI-master/src/opendiabetesvaultgui/slice/SliceController.java
+++ BP-OpenDiabetesVault-GUI-master/src/opendiabetesvaultgui/slice/SliceController.java
@@ -9,40 +9,129 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import opendiabetesvaultgui.launcher.FatherController;
 
 /**
  *
  * @author Daniel Schäfer, Martin Steil, Julian Schwind, Kai Worsch
  */
-public class SliceController extends FatherController implements Initializable{
+public class SliceController extends FatherController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorpane;
     
     @FXML
-    private ListView<String> listviewfilterelements ;
-    
-    
+    private ListView<String> listviewfilterelements;
+
     @FXML
-    private void doFilter(ActionEvent event){
-        
+    private Pane filtercombinationfield;
+
+    @FXML
+    private void doFilter(ActionEvent event) {
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
         //addItems to listview
         ObservableList<String> items = listviewfilterelements.getItems();
-        items.add("Test");
-        items.add("Test1");
-        items.add("Test2");
-        items.add("Test3");
-        items.add("Test4");
-        items.add("Test5");
-        
+        items.add("AndFilter");
+        items.add("OrFilter");
+        items.add("TimestampFilter");
+        items.add("TypeFilter");
+
+        //Dragevent for Listview
+        listviewfilterelements.setOnDragDetected(new EventHandler<MouseEvent>() {
+
+            public void handle(MouseEvent event) {
+                Dragboard dragBoard = listviewfilterelements.startDragAndDrop(TransferMode.ANY);
+
+                ClipboardContent content = new ClipboardContent();
+                content.putString(listviewfilterelements.getSelectionModel().getSelectedItem());
+                dragBoard.setContent(content);
+
+                event.consume();
+            }
+        });
+
+        //Nach dem Drag and drop
+        listviewfilterelements.setOnDragDone(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {               
+              
+            }
+        });
+
+        //DragOver for scrollpane
+        filtercombinationfield.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != filtercombinationfield
+                        && event.getDragboard().hasString()) {
+                    event.acceptTransferModes(TransferMode.ANY);
+                }
+
+                event.consume();
+            }
+        });
+
+        //Drag Bereich betreten
+        filtercombinationfield.setOnDragEntered(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != filtercombinationfield
+                        && event.getDragboard().hasString()) {
+
+                }
+
+                event.consume();
+            }
+        });
+
+        //Drag Bereich verlassen
+        filtercombinationfield.setOnDragExited(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                /* mouse moved away, remove the graphical cues */
+
+                event.consume();
+            }
+        });
+
+        //Drag loslassen
+        filtercombinationfield.setOnDragDropped(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    Label test = new Label();
+                    test.setText(db.getString());
+                    test.setLayoutX(event.getScreenX());
+                    test.setLayoutY(event.getScreenY()-110);
+                    
+                    anchorpane.getChildren().add(test);
+                    success = true;
+                }
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
+
     }
 
-    
-    
 }
